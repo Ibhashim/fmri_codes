bc05f52917be20b8c4a3430ca94129aef36f4114
Ibrahim Hashim <53156620+Ibhashim@users.noreply.github.com>
2019-08-07 18:28:09 +0200

    Update fitSingleTrial.py

--- fitSingleTrial.py
+++ fitSingleTrial.py
@@ -1,3 +1,9 @@
+# fmri_single_trial_fit
+#This code is my rewrite of a MATLAB code by one of my supervisors at university. 
+#The code returns the beta and T value for the fit of the voxel and hrf time courses. It takes as input two lists; the voxel time course and the hrf time course. 
+#The code is 1/100 the speed of the MATLAB code. If anyone has a suggestion on how to improve the speed of the code I would love to hear!
+
+
 def single_trial_fit(X, hrf): # X is timecourse of voxel, hrf is hrf timecourse over same time
     X = np.array(X).reshape(len(X),1) # add voxel time course as list - converst to np.array column vector
     hrf = np.array(hrf).reshape(len(hrf),1) # add hrf as list - converts to np.array column vector
